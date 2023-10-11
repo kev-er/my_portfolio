2582e83d84e1ab7f6370bf05ec90544062540a61
Kevin <holmk21@gmail.com>
2023-10-11 11:32:48 -0400

    updateting portfolio

---
+++ src/components/pages/homepage-comps/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import { Link as ScrollLink } from 'react-scroll';
+
+const About = () => {
+    return (
+        <section name="About" 
+        className='relative w-full md:h-screen text-white h-unset'>
+
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-lg'>
+
+                    <div className='pb-8'>
+                        <h2 className='text-4xl sm:text-5xl font-bold inline border-b-4 border-primary-color/40'>About me</h2>
+                    </div>
+
+                    <p className="mb-4 py-6">Hi! I'm a self-taught Frontend Developer. In early 2020 I started learning how to code and discovered my love for coding & Linux. Learning is a big part of who I am. I love expanding my knowledge base and gaining as much expertise as I can in any given field. I am currently learning Neovim as my IDE and learning back-end with Nodejs </p>
+
+
+                    <Link to='/about-me' className='text-black font-semibold text-[16px] w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-t from-green-400 to-primary-color cursor-pointer mx-auto md:mx-0 self-end mt-8 hover:scale-110 duration-300'>
+                        See more
+                        <span className=''><MdOutlineKeyboardArrowRight size={25} className='ml-1' /></span>
+                    </Link>
+                
+            </div>
+
+            <ScrollLink to="Projects" smooth duration={500} className='absolute bottom-2 -left-full md:left-1/2 md:-translate-x-1/2 cursor-pointer hover:text-primary-color'>
+                <i className='bx bx-chevron-down text-7xl text-gray-400 animate-bounce font hover:text-primary-color'></i>
+            </ScrollLink>
+            
+        </section>
+        
+    )
+}
+
+export default About
---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+const projects = [
+  {
+    id: 1,
+    title: "Portfolio",
+    description: "A place to showcase my work",
+    technologies: "Javascript, React, Tailwind CSS",
+    src: "/images/projectsImages/portfolio.webp",
+    demo: "https://easyshop-bydt.netlify.app/",
+    code: "https://github.com/diegotellezc/EasyShop",
+  },
+  {
+    id: 2,
+    title: "Ash Kennels",
+    description:
+      "A dog kennel wibsite create wit Html, CSS, Javascript and bootstrap.",
+    technologies: "Javascript, HTML5,  CSS, bootstrap",
+    src: "/images/projectsImages/ash_kennels.webp",
+    demo: "https://stately-bunny-f1e8bf.netlify.app/",
+    code: "https://github.com/kev-er/Ash_Kennels",
+  },
+  {
+    id: 3,
+    title: "Professionnel Mentor website",
+    description:
+      "A sleek and minimalistic website designed for a professional mentor, targeting prospective clients. ",
+    technologies: "Javascript, HTML5, CSS3",
+    src: "/images/projectsImages/mentor-website.webp",
+    demo: "https://gilded-raindrop-d16b8f.netlify.app/",
+    code: "https://github.com/kev-er/stronger-together-mentoring",
+  },
+  {
+    id: 4,
+    title: "Razor King",
+    description:
+      "A straightforward barber website with a strong emphasis on responsiveness,speed, and user-friendly navigation.",
+    technologies: "HTML5, CSS3, Javascript",
+    src: "/images/projectsImages/razor_king.webp",
+    demo: "https://cute-piroshki-88d7f7.netlify.app/",
+    code: "https://github.com/kev-er/razor_king_barber",
+  },
+
+];
+
+export default projects;
