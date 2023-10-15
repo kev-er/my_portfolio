04b6718d959526a193d6b9b76ea36dc9fa38514b
Kevin <holmk21@gmail.com>
2023-10-15 18:15:11 -0400

    changing contact form

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # My portfolio
-A place to showcase my work 
+
 ![portfolio](https://github.com/kev-er/my_portfolio/assets/81853888/301d5476-de05-4c9d-b62e-e186859040dd)
 
 **Link to project:** https://kevin-holm.netlify.app/
 
-![alt tag](http://placecorgi.com/1200/650)
 
+A place to showcase my work 
 ## How It's Made:
 
-**Tech used:** HTML, Tailwind CSS, JavaScript, React
+**Tech used:** Tailwind CSS, JavaScript, React
 
 
 
--- src/components/pages/homepage-comps/Contact.jsx
+++ src/components/pages/homepage-comps/Contact.jsx
@@ -1,66 +1,91 @@
-import React from 'react'
-import axios from 'axios';
-import Swal from 'sweetalert2'
-
+import React from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const formData = new FormData(event.target);
 
-        const formData = new FormData(event.target);
-
-        axios.post('https://getform.io/f/feff19e5-27a9-4f74-be5d-9699c4b245ed', formData)
-            .then(response => {
-                Swal.fire({
-                    icon: 'success',
-                    iconColor: '#0DFC4B',
-                    title: 'Thank you for contacting me. It will be a pleasure to work with you!',
-                    showConfirmButton: true,
-                    background: '#191a19',
-                    color: '#fff',
-                    confirmButtonColor: '#117911',
-                    backdrop: `
+    axios
+      .post(
+        "https://getform.io/f/e585857f-b9cc-4497-97d9-f12629757d71",
+        formData,
+      )
+      .then((response) => {
+        Swal.fire({
+          icon: "success",
+          iconColor: "#0DFC4B",
+          title:
+            "Thank you for contacting me. I will get in contact with you shortly!",
+          showConfirmButton: true,
+          background: "#191a19",
+          color: "#fff",
+          confirmButtonColor: "#117911",
+          backdrop: `
                         rgba(54, 55, 54,0.4)
-                    `
-                })
-            })
-            .catch(error => {
-            console.log(error);
-            });
-        
-        event.target.reset();
-    }
-    
+                    `,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    return (
-        <section name='Contact' className='relative w-full md:h-screen p-4 text-white h-unset'>
-            <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
-                <div className='pb-8'>
-                    <h2 className='text-4xl font-bold inline border-b-4 border-primary-color/40 sm:text-5xl'>Contact</h2>
-                    <p className='py-6'>Submit the form below to get in touch with me</p>
-                </div>
+    event.target.reset();
+  };
 
-                <div className='flex justify-center items-center'>
-                    <form onSubmit={handleSubmit} className='flex flex-col w-full md:w-1/2'>
-                        <input 
-                        type="text" 
-                        name='name' placeholder='Enter your name' 
-                        className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color' required/>
+  return (
+    <section
+      name="Contact"
+      className="relative w-full md:h-screen p-4 text-white h-unset"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <h2 className="text-4xl font-bold inline border-b-4 border-primary-color/40 sm:text-5xl">
+            Contact
+          </h2>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
 
-                        <input 
-                        type="email" 
-                        name='email' placeholder='Enter your email' 
-                        className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color' required />
+        <div className="flex justify-center items-center">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color"
+              required
+            />
 
-                        <textarea name="message" rows="10" placeholder='Enter your message' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color' required></textarea>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color"
+              required
+            />
 
-                        <button className='text-black font-semibold bg-gradient-to-t from-green-400 to-primary-color px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
-                    </form>
-                </div>
-            </div>
-        </section>
-    )
-}
+            <textarea
+              name="message"
+              rows="10"
+              placeholder="Enter your message"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none focus:border-primary-color"
+              required
+            ></textarea>
 
-export default Contact
+            <button className="text-black font-semibold bg-gradient-to-t from-green-400 to-primary-color px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
+
